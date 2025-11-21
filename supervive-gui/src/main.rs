14ac555a5f9f2fb547384fa4830358ce7fc6610b
lucannez64 <hery.dannez@gmail.com>
2025-11-21 15:37:36 +0100

    feat: implement game search and match data caching, updating dependencies.

--- supervive-gui/src/main.rs
+++ supervive-gui/src/main.rs
@@ -5,9 +5,7 @@ mod views;
 use api::SuperviveService;
 use gpui::prelude::*;
 use gpui::*;
-use reqwest_client::ReqwestClient;
 use state::AppState;
-use std::sync::Arc;
 use views::root::RootView;
 use views::search::SearchView;
 
@@ -15,10 +13,6 @@ fn main() {
     env_logger::init();
 
     Application::new().run(|cx| {
-        // Configure HTTP client for remote image loading
-        let http_client = ReqwestClient::user_agent("supervive-gui").unwrap();
-        cx.set_http_client(Arc::new(http_client));
-
         let service = SuperviveService::new().expect("Failed to initialize service");
         let app_state = AppState::new(service);
         cx.set_global(app_state);
